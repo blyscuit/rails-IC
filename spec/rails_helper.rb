59dd151997cfcebe026ec25619eb8b5842754d71
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-05-30 09:24:43 +0700

    [#2] Add spec_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 require 'spec_helper'
- require 'rspec/rails'
- require 'json_matchers/rspec'
- require 'pundit/rspec'
- require 'support/simplecov'
+require 'rspec/rails'
+require 'json_matchers/rspec'
+require 'pundit/rspec'
+require 'support/simplecov'
