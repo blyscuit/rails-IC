ef829141ef2bca7cfc6274803c8fb4334b25b8eb
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-05-31 18:29:09 +0700

    [#5] Add devise for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   namespace :api do
     namespace :v1 do
       resources :keywords, only: :index
     end
   end
+
+  resources :public, only: :index
+  resources :private, only: :index
 end
