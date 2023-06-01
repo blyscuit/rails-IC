3c6d707010a436dcbb438451940e373311f9a865
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-01 18:44:05 +0700

    [#5] Fix lint

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Init' do
-    before(:each) { @user = Fabricate(:user) }
+    let(:user) { Fabricate(:user) }
 
     it 'has email' do
-      expect(@user.email).not_to be_nil
+      expect(user.email).not_to be_nil
     end
   end
 end
