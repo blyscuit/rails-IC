e6b9b5135232d9c64ae35ce2366b4865cd8e09d5
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-01 14:36:30 +0700

    [#2] Add codebase_spec for local run because Danger runs most spec for comments

--- spec/codebase/codebase_local_spec.rb
+++ spec/codebase/codebase_local_spec.rb
@@ -3,6 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Codebase', codebase: true do
+  it 'does NOT offend Rubocop' do
+    expect(`rubocop --parallel --format simple`).to include 'no offenses detected'
+  end
+
+  it 'satisfies Brakeman' do
+    expect(`brakeman -w1`).not_to include '+SECURITY WARNINGS+'
+  end
+
   it 'does NOT break zeitwerk loading' do
     expect(`bundle exec rake zeitwerk:check`).to be_empty.or include 'All is good!'
   end
