6dbca5323cad51ac8d51d46bc431e38f8659261b
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-02 10:07:31 +0700

    [#2] Fix naming spec

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'String' do
-  context 'when converting to integer' do
-    it 'has correct value' do
+  context 'given a string number' do
+    it 'returns an integer' do
       expect('100'.to_i).to eq(100)
     end
   end
