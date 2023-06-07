7e5d89e8ac1db2935cae6d8f322c49be4daf46be
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-07 15:38:30 +0700

    [#6] Rename private_items to match convention

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :keywords, only: :index
-      get 'private/sample', to: 'private#sample'
+      get 'private/sample', to: 'private_items#index'
     end
   end
 
