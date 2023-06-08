833bb473f9005ab345b8044508f8df10b3a8dc28
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-08 10:10:11 +0700

    [#7] Convert controller specs to request

--- spec/routing/tokens_controller_spec.rb
+++ spec/routing/tokens_controller_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::TokensController, type: :routing do
   describe 'POST#login' do
     it 'routes to token#create' do
-       expect(post: 'api/v1/login').to route_to('api/v1/tokens#create')
+      expect(post: api_v1_login_path).to route_to('api/v1/tokens#create')
     end
   end
 end
