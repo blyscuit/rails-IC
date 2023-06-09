aae8139d75306eae80c5a2439a9ddac15f270c4b
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-09 18:35:28 +0700

    [#9] Add upload csv backend

---
+++ db/migrate/20230609095933_keyword_user.rb
@@ -0,0 +1,9 @@
+class KeywordUser < ActiveRecord::Migration[7.0]
+  def change
+    create_table :keyword_users do |t|
+      t.references :keyword, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
