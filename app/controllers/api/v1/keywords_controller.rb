276bd6103055d452d296063e39595b60ed63b397
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-14 15:27:27 +0700

    [#10] Add endpoint for uploading csv

--- app/controllers/api/v1/keywords_controller.rb
+++ app/controllers/api/v1/keywords_controller.rb
@@ -3,13 +3,35 @@
 module Api
   module V1
     class KeywordsController < ApplicationController
-      skip_before_action :doorkeeper_authorize!
-
       def index
         first_keyword = Keyword.new({ id: 1, name: 'First keyword' })
         second_keyword = Keyword.new({ id: 2, name: 'Second keyword' })
 
         render json: KeywordSerializer.new([first_keyword, second_keyword]).serializable_hash.to_json
+      end
+
+      def create
+        if csv_form.save(params[:file])
+          render json: create_success_response
+        else
+          render_errors(
+            details: csv_form.errors.full_messages,
+            code: :invalid_file,
+            status: :unprocessable_entity
+          )
+        end
+      end
+
+      private
+
+      def csv_form
+        @csv_form ||= CsvUploadForm.new(current_user)
+      end
+
+      def create_success_response
+        {
+          meta: I18n.t('csv.upload_success')
+        }
       end
     end
   end
---
+++ app/controllers/concerns/api/v1/error_handler_concern.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module ErrorHandlerConcern
+      extend ActiveSupport::Concern
+
+      private
+
+      def render_error(detail, source: nil, status: :unprocessable_entity)
+        error = build_error(detail: detail, source: source)
+        render_errors [error], status
+      end
+
+      def render_errors(jsonapi_errors, status = :unprocessable_entity)
+        render json: { errors: jsonapi_errors }, status: status
+      end
+    end
+  end
+end
