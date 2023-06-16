5469432f3591c540e27a1a5183e99767bdf0f6dd
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-16 12:44:00 +0700

    [#9] Add validation for saving keyword

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,6 @@ class User < ApplicationRecord
     user = User.find_for_authentication(email: email)
     user&.valid_password?(password) ? user : nil
   end
+
+  has_many :keywords, inverse_of: :user, dependent: :destroy
 end
