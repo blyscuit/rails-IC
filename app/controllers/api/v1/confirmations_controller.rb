340d3fe05be44408996f1a9f2ba7b9b2ac1e909c
Phong Vo <phong.d@nimblehq.co>
2023-06-19 09:05:50 +0700

    [#40] Configurate routes

--- app/controllers/api/v1/confirmations_controller.rb
+++ app/controllers/api/v1/confirmations_controller.rb
@@ -2,6 +2,10 @@
 
 module Api
   module V1
-    class ConfirmationsController < Devise::ConfirmationsController; end
+    class ConfirmationsController < Devise::ConfirmationsController
+      def after_confirmation_path_for(_resource_name, _resource)
+        success_messages_path
+      end
+    end
   end
 end
