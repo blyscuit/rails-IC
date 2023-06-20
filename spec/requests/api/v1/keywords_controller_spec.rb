cff3cf656db5feb4c2571d96ccb358b1f9a96123
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-20 11:54:10 +0700

    [#10] Update specs. Change naming for abbreviation

--- spec/requests/api/v1/keywords_controller_spec.rb
+++ spec/requests/api/v1/keywords_controller_spec.rb
@@ -3,17 +3,13 @@
 RSpec.describe Api::V1::KeywordsController, type: :request do
   describe 'GET#index' do
     it 'returns success status' do
-      header, = create_token_header
-
-      get api_v1_keywords_path, headers: header
+      get api_v1_keywords_path, headers: create_token_header
 
       expect(response).to have_http_status(:success)
     end
 
     it 'returns the valid keywords' do
-      header, = create_token_header
-
-      get api_v1_keywords_path, headers: header
+      get api_v1_keywords_path, headers: create_token_header
 
       keywords = JSON.parse(response.body, symbolize_names: true)[:data]
 
@@ -23,61 +19,54 @@ RSpec.describe Api::V1::KeywordsController, type: :request do
   end
 
   describe 'POST#index' do
-    context 'when csv file is valid' do
+    context 'when CSV file is valid' do
       it 'returns success status' do
-        header, = create_token_header
         params = { 'file' => fixture_file_upload('csv/valid.csv') }
-        post api_v1_keywords_path, params: params, headers: header
+        post api_v1_keywords_path, params: params, headers: create_token_header
 
         expect(response).to have_http_status(:success)
       end
 
       it 'saves 10 keywords to the DB' do
-        header, = create_token_header
         params = { 'file' => fixture_file_upload('csv/valid.csv') }
-        post api_v1_keywords_path, params: params, headers: header
+        post api_v1_keywords_path, params: params, headers: create_token_header
 
-        expect { post api_v1_keywords_path, params: params, headers: header }.to change(Keyword, :count).by(10)
+        expect { post api_v1_keywords_path, params: params, headers: create_token_header }.to change(Keyword, :count).by(10)
       end
 
       it 'returns the upload_success meta message' do
-        header, = create_token_header
         params = { 'file' => fixture_file_upload('csv/valid.csv') }
-        post api_v1_keywords_path, params: params, headers: header
+        post api_v1_keywords_path, params: params, headers: create_token_header
 
         expect(JSON.parse(response.body)['meta']).to eq(I18n.t('csv.upload_success'))
       end
     end
 
-    context 'when csv file is missing' do
+    context 'when CSV file is missing' do
       it 'returns unprocessable_entity status' do
-        header, = create_token_header
-        post api_v1_keywords_path, headers: header
+        post api_v1_keywords_path, headers: create_token_header
 
         expect(response).to have_http_status(:unprocessable_entity)
       end
 
       it 'returns a invalid_file error' do
-        header, = create_token_header
-        post api_v1_keywords_path, headers: header
+        post api_v1_keywords_path, headers: create_token_header
 
         expect(JSON.parse(response.body)['errors']['code']).to eq('invalid_file')
       end
     end
 
-    context 'when csv file is in the wrong type' do
+    context 'when CSV file is in the wrong type' do
       it 'returns unprocessable_entity status' do
-        header, = create_token_header
         params = { 'file' => fixture_file_upload('csv/wrong_type.txt') }
-        post api_v1_keywords_path, params: params, headers: header
+        post api_v1_keywords_path, params: params, headers: create_token_header
 
         expect(response).to have_http_status(:unprocessable_entity)
       end
 
-      it 'returns a wrong_type error' do
-        header, = create_token_header
+      it 'returns the wrong_type error' do
         params = { 'file' => fixture_file_upload('csv/wrong_type.txt') }
-        post api_v1_keywords_path, params: params, headers: header
+        post api_v1_keywords_path, params: params, headers: create_token_header
 
         expect(JSON.parse(response.body)['errors']['details']).to include(I18n.t('csv.validation.wrong_type'))
       end
