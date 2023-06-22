cec47ca53c3e9c591cff3bd0235a465b3fc66cae
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-22 18:20:58 +0700

    [#18] Add detail serializer

---
+++ app/serializers/keyword_detail_serializer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class KeywordDetailSerializer < ApplicationSerializer
+  attributes :name
+  attributes :ads_top_count
+  attributes :ads_page_count
+  attributes :ads_top_urls
+  attributes :result_count
+  attributes :result_urls
+  attributes :total_link_count
+  attributes :html
+  attributes :source
+end
