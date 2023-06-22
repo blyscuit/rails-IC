a27c010ed7648f0dee6336a5ba054241ce6a42e0
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-22 18:20:58 +0700

    [#18] Add detail serializer

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
         controllers tokens: 'tokens'
         skip_controllers :authorizations, :applications, :authorized_applications, :token_info, :tokens, :confirmations
       end
-      resources :keywords, only: [:index, :create]
+      resources :keywords, only: [:index, :create, :show]
       resources :private_items, only: :index
       resources :tokens, only: [:create]
       resources :registrations, only: [:create]
