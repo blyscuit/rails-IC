d11a4fbbc1c39b050c0ca6e1524717f7dc57bf73
Phong Vo <phong.d@nimblehq.co>
2023-06-26 11:13:27 +0700

    [#11] Add pundit policy

--- app/policies/keyword_policy.rb
+++ app/policies/keyword_policy.rb
@@ -6,4 +6,18 @@ class KeywordPolicy < ApplicationPolicy
       scope.where(keywords: { user_id: user.id })
     end
   end
+
+  def index?
+    user_valid?
+  end
+
+  def create?
+    user_valid?
+  end
+
+  private
+
+  def user_valid?
+    !user.id.nil? && !user.email.nil?
+  end
 end
