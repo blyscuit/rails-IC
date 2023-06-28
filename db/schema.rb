b04eb7d46ef144eb36b828a86cd9c4ad665cef6e
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-28 12:28:44 +0700

    [#64] Add unique and case-insensitive to Sources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_20_095911) do
+ActiveRecord::Schema.define(version: 2023_06_28_044856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,9 +62,10 @@ ActiveRecord::Schema.define(version: 2023_06_20_095911) do
   end
 
   create_table "sources", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_sources_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
