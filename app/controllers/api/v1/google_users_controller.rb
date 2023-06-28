4ddb0babb9d5227017f94c3aa4ed2db7788f314f
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-28 16:22:48 +0700

    [#48] Add Google Auth service

---
+++ app/controllers/api/v1/google_users_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class GoogleUsersController < ApplicationController
+
+      skip_before_action :doorkeeper_authorize!, only: %i[create]
+
+      def create
+        g = ExternalAuth::GoogleAuth.new(params[:code])
+        data = g.get_user!
+        if data.present?
+        user = User.new(email: data['email'], password: data['sub'], login_type: :google)
+          if user.save
+            render json: 'Success'
+          else
+            render(json: { error: user.errors.full_messages }, status: 422)
+          end
+        else
+          render(json: { error: 'Invalid request' }, status: 422)
+        end
+      end
+    end
+  end
+end
---
+++ app/services/external_auth/fetch_google_token_service.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+module ExternalAuth
+  class FetchGoogleTokenService
+    AUTH_CODE_URL = 'https://www.googleapis.com/oauth2/v3/token'
+    ACCESS_TOKEN_URL = 'https://www.googleapis.com/oauth2/v3/userinfo?access_token='
+    GRANT_TYPE = 'authorization_code'
+
+    def initialize(access_code)
+      @access_code = access_code
+    end
+
+    def call
+      @access_token ||= fetch_access_token
+      return unless access_token
+      google = URI("#{ACCESS_TOKEN_URL}#{access_token}")
+      response = HTTParty.get(google)
+      body = JSON.parse(response.body)
+
+      {
+        email: body['email'],
+        sub: body['sub']
+      }
+    end
+
+    private
+
+    attr_reader :access_token
+
+    def app_auth_data
+      {
+        client_id: Rails.application.credentials.dig(:google_oauth, :client_id),
+        client_secret: Rails.application.credentials.dig(:google_oauth, :client_secret),
+        redirect_uri: Rails.application.credentials.dig(:google_oauth, :redirect_uri)
+      }
+    end
+
+    def fetch_access_token
+      uri = URI(AUTH_CODE_URL)
+      params = {
+        code: @access_code,
+        client_id: app_auth_data[:client_id],
+        client_secret: app_auth_data[:client_secret],
+        redirect_uri: app_auth_data[:redirect_uri],
+        grant_type: GRANT_TYPE
+      }
+      response = HTTParty.post(uri, body: params)
+      data = JSON.parse(response.body)
+      data['access_token'] if data['access_token']
+    end
+  end
+end
