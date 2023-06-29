ac0fb763c862a8bba0eafee0448bb2a2c8718489
Phong Vo <phong.d@nimblehq.co>
2023-06-29 13:55:19 +0700

    [#34] Remove temporary endpoints

--- app/controllers/private_items_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class PrivateItemsController < ApplicationController
-  before_action :doorkeeper_authorize!
-
-  def index
-    render json: 'Welcome back'
-  end
-end
