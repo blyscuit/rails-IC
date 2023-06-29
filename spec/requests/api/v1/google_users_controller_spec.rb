d7052c8441b6c687b9b7d84f0c9b26a114e51467
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-29 10:38:52 +0700

    [#48] Add Google Auth register controller

---
+++ spec/requests/api/v1/google_users_controller_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::GoogleUsersController, type: :request do
+  describe 'POST#create' do
+    context 'given valid credentials' do
+      it 'returns http status of created' do
+        params = { :code => 'code' }
+        ACCESS_TOKEN = 'access_token'
+        RESULT_BODY = '{ "email": "email@email.com", "sub": "123456" }'
+        stub_request(:post, %r{googleapis.com/oauth2/v3}).to_return(body: "{ \"access_token\": \"#{ACCESS_TOKEN}\" }")
+        stub_request(:get, %r{googleapis.com/oauth2/v3}).to_return(body: RESULT_BODY)
+        post api_v1_google_users_path, params: params
+
+        expect(response).to have_http_status(:created)
+      end
+    end
+  end
+end
