ca2d913caf0d7ffc6c02dd922b31f7ec551e5fc7
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-29 12:51:17 +0700

    [#48] Add sub as authen method

--- spec/requests/api/v1/google_users_controller_spec.rb
+++ spec/requests/api/v1/google_users_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Api::V1::GoogleUsersController, type: :request do
   describe 'POST#create' do
     context 'given valid credentials' do
       it 'returns http status of created' do
-        params = { :code => 'code' }
+        params = { code: 'code' }
         ACCESS_TOKEN = 'access_token'
         RESULT_BODY = '{ "email": "email@email.com", "sub": "123456" }'
         stub_request(:post, %r{googleapis.com/oauth2/v3}).to_return(body: "{ \"access_token\": \"#{ACCESS_TOKEN}\" }")
