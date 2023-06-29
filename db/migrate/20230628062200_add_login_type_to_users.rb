005a4cade65b5bf931ff644ac84748f710b67e8b
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-29 12:51:17 +0700

    [#48] Add sub as authen method

--- db/migrate/20230628062200_add_login_type_to_users.rb
+++ db/migrate/20230628062200_add_login_type_to_users.rb
@@ -2,6 +2,7 @@ class AddLoginTypeToUsers < ActiveRecord::Migration[7.0]
   def change
     change_table :users do |t|
       t.string :login_type, default: 'email', null: false
+      t.string :sub, default: '', null: false
     end
   end
 end
