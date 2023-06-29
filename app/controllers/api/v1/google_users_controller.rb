f92758f40b414e2260830832de09a7d2f122471c
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-29 10:38:52 +0700

    [#48] Add Google Auth register controller

--- app/controllers/api/v1/google_users_controller.rb
+++ app/controllers/api/v1/google_users_controller.rb
@@ -3,24 +3,26 @@
 module Api
   module V1
     class GoogleUsersController < ApplicationController
-
       skip_before_action :doorkeeper_authorize!, only: %i[create]
 
       def create
-        google_auth_service = ExternalAuth::GoogleAuth.new(params[:code])
-        data = google_auth_service.get_user
-        render_errors(
+        return render_errors(
+          details: I18n.t('authetication.generic_error'),
+          status: :bad_request
+        ) unless params[:code]
+        data = ExternalAuth::FetchGoogleTokenService.new(params[:code]).call
+        return render_errors(
           details: I18n.t('authetication.generic_error'),
           status: :unprocessable_entity
         ) unless data
 
         user = User.new(email: data[:email], password: data[:sub], login_type: :google)
-        render_errors(
+        return render_errors(
           details: user.errors.full_messages,
           status: :unprocessable_entity
         ) unless user.save
 
-        render success: true, status: :login
+        render success: true, status: :created
       end
     end
   end
