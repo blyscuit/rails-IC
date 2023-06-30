ac40739c594175c3381ebeed86b5f359ab037852
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-30 14:52:06 +0700

    [#48] Add login option and return token

--- app/controllers/api/v1/users/omniauth_callbacks_controller.rb
+++ app/controllers/api/v1/users/omniauth_callbacks_controller.rb
@@ -30,10 +30,24 @@ module Api
           @auth ||= request.env['omniauth.auth']
         end
 
-        def render_success
-          # TODO: Return JWT token
+        def client_application
+          state = JSON.parse(request.env['action_dispatch.request.parameters']['state'])
+          app_client_id = state['client_id']
 
-          render json: { success: true }
+          Doorkeeper::Application.find_by(uid: app_client_id)
+        end
+
+        def render_success
+          user = @user
+          access_token = Doorkeeper::AccessToken.create(
+            resource_owner_id: user.id, 
+            application_id: client_application.id, 
+            use_refresh_token: true, 
+            expires_in: Doorkeeper.configuration.access_token_expires_in.to_i, 
+            scopes: ''
+          )
+          
+          render json: Doorkeeper::OAuth::TokenResponse.new(access_token).body
         end
       end
     end
