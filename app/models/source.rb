31b9bd28333d90f7ce918b5abcf4c333308c1ecc
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-07-04 15:41:11 +0700

    [#64] Add naming check to avoid db failure

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Source < ApplicationRecord
+  include Discard::Model
+
   has_many :keywords, inverse_of: :source, dependent: :nullify
 
   validates :name, uniqueness: { case_sensitive: false }
