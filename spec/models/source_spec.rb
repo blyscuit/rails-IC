b11d4b752f640a96c4459e4fc0d552476e16a355
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-07-05 14:56:49 +0700

    [#64] Make spec description match

--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -4,22 +4,20 @@ require 'rails_helper'
 
 RSpec.describe Source, type: :model do
   context 'when creating two Sources with different cases' do
-    it 'does NOT create a second source' do
-      described_class.find_or_create_by({ name: 'nimble' })
-
+    it 'saves 1 source' do
       expect do
+        described_class.find_or_create_by({ name: 'nimble' })
         described_class.find_or_create_by({ name: 'nImBle' })
-      end.not_to change(described_class, :count)
+      end.to change(described_class, :count).by(1)
     end
   end
 
   context 'when creating two Sources with different names' do
     it 'saves 2 sources' do
-      described_class.find_or_create_by({ name: 'nimble1' })
-
       expect do
+        described_class.find_or_create_by({ name: 'nimble1' })
         described_class.find_or_create_by({ name: 'nimble2' })
-      end.to change(described_class, :count).by(1)
+      end.to change(described_class, :count).by(2)
     end
   end
 end
