63acee6cbef290fd5d3de0b474e3b923634dca5e
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-07-05 16:59:02 +0700

    [#49] Add enum constraint to user

--- db/migrate/20230628062200_add_provider_to_users.rb
+++ db/migrate/20230628062200_add_provider_to_users.rb
@@ -1,7 +1,7 @@
 class AddProviderToUsers < ActiveRecord::Migration[7.0]
   def change
     change_table :users do |t|
-      t.string :provider
+      t.string :provider, default: "email"
       t.string :uid
     end
   end
