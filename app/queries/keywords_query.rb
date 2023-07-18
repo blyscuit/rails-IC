1b1170814ea9c0bd3900240c7ece8685f9d3ae18
Phong Vo <phong.d@nimblehq.co>
2023-07-18 22:38:17 +0700

    [#24] Add more test cases

--- app/queries/keywords_query.rb
+++ app/queries/keywords_query.rb
@@ -9,24 +9,31 @@ class KeywordsQuery
   end
 
   def call
-    if filter_params[:adwords_url_contains]
-      filter_ads_top_urls(filter_params[:adwords_url_contains])
-    elsif filter_params[:word] && filter_params[:match_at_least]
-      filter_result_urls(filter_params[:word], filter_params[:match_at_least])
-    else
-      scope
-    end
+    return filter_ads_top_urls if filter_params[:adwords_url_contains]
+
+    return filter_result_urls if filter_params[:word] && filter_params[:match_at_least]
+
+    scope
   rescue NoMethodError
     scope
   end
 
   private
 
-  def filter_ads_top_urls(word)
+  def filter_ads_top_urls
+    word = filter_params[:adwords_url_contains]&.downcase
+
+    return scope unless word
+
     scope.where("array_to_string(ads_top_urls, '||') ILIKE ?", "%#{word}%")
   end
 
-  def filter_result_urls(word, number_of_matches)
+  def filter_result_urls
+    word = filter_params[:word]
+    number_of_matches = filter_params[:match_at_least]
+
+    return scope if word.blank? || number_of_matches.blank?
+
     sub_query = Keyword.select('sub_table.id, sub_table.user_id')
                        .from(Keyword.select('id, user_id, unnest(result_urls) as result_url'), 'sub_table')
                        .where('result_url ~* ?', "(#{word}.*){#{number_of_matches},}")
