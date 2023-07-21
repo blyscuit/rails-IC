4ac4b9165e1238d89f383720f517c731ef5ec63c
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-07-21 11:17:56 +0700

    [#22] Remove unused keyword attr

--- app/queries/keywords_query.rb
+++ app/queries/keywords_query.rb
@@ -12,30 +12,28 @@ class KeywordsQuery
 
   private
 
-  attr_reader :scope, :filter
-  attr_accessor :keywords
+  attr_reader :filter
+  attr_accessor :scope
 
   def filter_urls
     return scope unless filter
 
-    @keywords = scope
+    @scope = filter_ads_top_urls if filter[:adwords_url_contains].present?
+    @scope = filter_result_urls if filter[:result_url_contains].present?
+    @scope = filter_match_at_least if filter[:word].present? && filter[:match_at_least].present?
 
-    @keywords = filter_ads_top_urls if filter[:adwords_url_contains].present?
-    @keywords = filter_result_urls if filter[:result_url_contains].present?
-    @keywords = filter_match_at_least if filter[:word].present? && filter[:match_at_least].present?
-
-    @keywords
+    scope
   end
 
   def filter_ads_top_urls
-    keywords.where("array_to_string(ads_top_urls, '||') ILIKE ?", "%#{filter[:adwords_url_contains]}%")
+    scope.where("array_to_string(ads_top_urls, '||') ILIKE ?", "%#{filter[:adwords_url_contains]}%")
   end
 
   def filter_result_urls
-    keywords.where("array_to_string(result_urls, '||') ILIKE ?", "%#{filter[:result_url_contains]}%")
+    scope.where("array_to_string(result_urls, '||') ILIKE ?", "%#{filter[:result_url_contains]}%")
   end
 
   def filter_match_at_least
-    keywords.where("array_to_string(result_urls, '||') ~* ?", "(#{filter[:word]}.*){#{filter[:match_at_least]},}")
+    scope.where("array_to_string(result_urls, '||') ~* ?", "(#{filter[:word]}.*){#{filter[:match_at_least]},}")
   end
 end
